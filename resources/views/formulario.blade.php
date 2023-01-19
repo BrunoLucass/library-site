f310bed9fdf8327961973e37851cf42f62b3ed19
BrunoLucass <bruno.2020109475@unicap.br>
2023-01-19 08:30:19 -0300

    atualizando o crud

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -1,85 +1,68 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Formulário</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-       
-        
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Formulário</title>
 
-    </head>
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <body>
-      
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+
+    {{-- formulario  --}}
+
     <div class="box">
-      <form action="" id="formulario">
-        <fieldset>
-          <legend><b>Fórmulário</b></legend>
-          <br />
-          <div class="inputBox">
-            <input
-              type="text"
-              name="title"
-              id="title"
-              class="inputUser"
-              required
-            />
-            <label for="titulo" class="labelInput">Titulo</label>
-          </div>
-          <br /><br />
-          <div class="inputBox">
-            <input
-              type="text"
-              name="author"
-              id="author"
-              class="inputUser"
-              required
-            />
-            <label for="autor" class="labelInput">Autor</label>
-          </div>
-          <br /><br />
-          <div class="inputBox">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              class="inputUser"
-              required
-            />
-            <label for="nome" class="labelInput">Nome</label>
-          </div>
-          <br /><br />
-          <label for="data_nascimento"><b>Data de Entrada:</b></label>
-          <input
-            type="date"
-            name="date"
-            id="date"
-            required
-          />
-         
-          <br /><br />
+        <form action="POST" id="formulario">
+            <fieldset>
+                <legend><b>Fórmulário</b></legend>
+                <br />
+                <div class="inputBox">
+                    <input type="text" name="title" id="title" class="inputUser" required />
+                    <label for="titulo" class="labelInput">Titulo</label>
+                </div>
+                <br /><br />
+                <div class="inputBox">
+                    <input type="text" name="author" id="author" class="inputUser" required />
+                    <label for="autor" class="labelInput">Autor</label>
+                </div>
+                <br /><br />
+                <div class="inputBox">
+                    <input type="text" name="name" id="name" class="inputUser" required />
+                    <label for="nome" class="labelInput">Nome</label>
+                </div>
+                <br /><br />
+                <label for="data_nascimento"><b>Data de Entrada:</b></label>
+                <input type="date" name="date" id="date" required />
 
-           <a href="{{route('welcome')}}">
 
-          <button type="button" name="submit" id="submit" onClick="" >Enviar</button>
+                {{-- botoes  --}}
 
-          </a> 
+                <br /><br />
 
-          <br /><br />
+                <a href="{{ route('welcome') }}">
 
-          <a href="{{route('welcome')}}">
+                    <button type="button" name="submit" id="submit" onClick="">Enviar</button>
 
-            <button type="button" name="submit" id="submit" onClick="" >Voltar</button>
-  
-            </a> 
+                </a>
 
-        </fieldset>
-      </form>
-    </body>
-    <script>
-</html>
+                <br /><br />
+
+                <a href="{{ route('welcome') }}">
+
+                    <button type="button" name="submit" id="submit" onClick="">Voltar</button>
+
+                </a>
+
+            </fieldset>
+        </form>
+</body>
+<script>
+    < /html>
